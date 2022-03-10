d68c73b891e15607f02978e8ec9917f795ece060
Pauline122112 <pauline.design007@gmail.com>
2022-03-10 13:35:25 -0600

    Finally able to connect navbar component to app.js

--- pauline-stokes/src/App.js
+++ pauline-stokes/src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import { Router, Routes, Route, Link, Outlet } from "react-router-dom";
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import './App.css'
 
 
 function App() {
   return (
-    <div className='App'>
-      <h1>HEY</h1>
-      </div>
-  );
+		<>
+			<Router>
+				<Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+			</Router>
+		</>
+	);
 }
 
-export default App;
+export default App
--- pauline-stokes/src/components/Navbar.js
+++ pauline-stokes/src/components/Navbar.js
@@ -5,17 +5,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 function Navbar() {
   return (
 		<>
-			<nav className='navbar'>
-				<div className='navbar-container'>
-					<Link to='/' className='navbar-logo'> 
-					{/* essentially the logo */}
-					TRVL
-					
+			<nav className="navbar">
+				<div className="navbar-container">
+					<Link to="/" className="navbar-logo">
+						{/* essentially the logo */}
+						TRVL <i class="fa-brands fa-typo3"></i>
 					</Link>
 				</div>
 			</nav>
 		</>
-	)
+	);
 }
 
 export default Navbar
