bf278c50b2e071a553cb80f379d8af9e554f1c12
Pauline122112 <pauline.design007@gmail.com>
2022-03-10 15:00:30 -0600

    adding hamburger menu to navbar.js

--- pauline-stokes/src/components/Navbar.js
+++ pauline-stokes/src/components/Navbar.js
@@ -2,9 +2,14 @@ import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faKiwiBird} from '@fortawesome/free-solid-svg-icons'
-import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+// import { faTypo3 } from '@fortawesome/free-brands-svg-icons'
 
 export default function Navbar() {
+	const [click, setClick] = useState(false)
+	// setting the state to update and change it from whatever it was to whatever I want to change it to- this is for the menu. 
+	const handleClick = () => setClick(!click)
+	//basically setting it to true or the opposite of what is in the useState
+	//Switches from hamburger icon to X
   return (
 		<>
 			<nav className="navbar">
@@ -13,12 +18,18 @@ export default function Navbar() {
 						{/* essentially the logo */}
 						PS 
 						<FontAwesomeIcon icon={faKiwiBird}></FontAwesomeIcon>
-						<FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon>
 					</Link>
+					<div className='menu-icon' onClick={handleClick}>
+						<i className={click ? 'fas fa-times' : 'fas fa-bars'}
+						 />
+						 {/* toggle if not clicked will be hamburger menu, on click will be the bars */}
+						</div>
 				</div>
 			</nav>
 		</>
 	);
 }
 
-// <i class="fa-brands fa-typo3"></i> = <FontAwesomeIcon icon={fa}></FontAwesomeIcon>
+// <i class="fa-brands fa-typo3"></i> = <FontAwesomeIcon icon={faTypo3}></FontAwesomeIcon>
+
+// menu-icon- burger menu- Will use state to change from an X to a hamburger menu
