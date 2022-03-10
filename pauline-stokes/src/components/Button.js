3eea5edc7396ba935f669943d9f2550cd8688e20
Pauline122112 <pauline.design007@gmail.com>
2022-03-10 15:39:34 -0600

    completed button component

--- pauline-stokes/src/components/Button.js
+++ pauline-stokes/src/components/Button.js
@@ -20,6 +20,10 @@ export const Button = ({
     return (
         <Link to='/work' className='btn-mobile'>
             <button>
+                className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+                onClick={onClick}
+                type={type}
+                
                 {children}
             </button>
         </Link>
