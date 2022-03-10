bf5f175b47ab60801f703d31d80b1c8b0ecccec8
Pauline122112 <pauline.design007@gmail.com>
2022-03-09 22:49:34 -0600

    Working on navbar

--- pauline-stokes/src/components/Navbar.js
+++ pauline-stokes/src/components/Navbar.js
@@ -1,17 +1,18 @@
-import React, {useState} from 'react'
-import link from 'react-router-dom';
-function navbar() {
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar() {
   return (
 		<>
 			<nav className="navbar">
 				<div className="navbar-container">
-					<link to="/" className="navbar-logo">
+					<Link to="/" className="navbar-logo">
 						TRVL <i className='fab fa-typo3'/>
-					</link>
+					</Link>
 				</div>
 			</nav>
 		</>
 	);
 }
 
-export default navbar
+export default Navbar
