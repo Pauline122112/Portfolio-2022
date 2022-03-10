ee86a43881018f9f67ee49c714c68a1afefde95b
Pauline122112 <pauline.design007@gmail.com>
2022-03-09 21:34:00 -0600

    implementing code to app and navbar

--- pauline-stokes/src/components/Navbar.js
+++ pauline-stokes/src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React, {useState} from 'react'
-import navbar from './Navbar';
-
+import {link} from 'react-router-dom'
 function navbar() {
   return (
     <>
-    <nav className='navbar'></nav>
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            <link to='/' className='navbar-logo'>
+                TRVL <i className='fab fa-typo3' />
+                </link>
+        
+        </div>
+    </nav>
     </>
   )
 }
