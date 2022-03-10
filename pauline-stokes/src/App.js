1d3cb3afdaad9852c88a15483310e33ec1e4ed30
Pauline122112 <pauline.design007@gmail.com>
2022-03-10 13:16:37 -0600

    trying to get the navbar component to work

--- pauline-stokes/src/App.js
+++ pauline-stokes/src/App.js
@@ -3,16 +3,12 @@ import Navbar from './components/Navbar';
 import { Router, Routes, Route, Link, Outlet } from "react-router-dom";
 import './App.css';
 
+
 function App() {
   return (
-    <>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' exact />
-      </Routes>
-    </Router>
-    </>
+    <div className='App'>
+      <h1>HEY</h1>
+      </div>
   );
 }
 
--- pauline-stokes/src/components/Navbar.js
+++ pauline-stokes/src/components/Navbar.js
@@ -1,18 +1,21 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function Navbar() {
   return (
 		<>
-			<nav className="navbar">
-				<div className="navbar-container">
-					<Link to="/" className="navbar-logo">
-						TRVL <i className='fab fa-typo3'/>
+			<nav className='navbar'>
+				<div className='navbar-container'>
+					<Link to='/' className='navbar-logo'> 
+					{/* essentially the logo */}
+					TRVL
+					
 					</Link>
 				</div>
 			</nav>
 		</>
-	);
+	)
 }
 
 export default Navbar
