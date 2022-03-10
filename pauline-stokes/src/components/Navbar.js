10bb93957e312e868518e31634f8345fb490a5b7
Pauline122112 <pauline.design007@gmail.com>
2022-03-10 15:25:33 -0600

    adding buttons

--- pauline-stokes/src/components/Navbar.js
+++ pauline-stokes/src/components/Navbar.js
@@ -10,20 +10,46 @@ export default function Navbar() {
 	const handleClick = () => setClick(!click)
 	//basically setting it to true or the opposite of what is in the useState
 	//Switches from hamburger icon to X
+	const closeMobileMenu = () => setClick(false)
   return (
 		<>
 			<nav className="navbar">
 				<div className="navbar-container">
 					<Link to="/" className="navbar-logo">
 						{/* essentially the logo */}
-						PS 
+						PS
 						<FontAwesomeIcon icon={faKiwiBird}></FontAwesomeIcon>
 					</Link>
-					<div className='menu-icon' onClick={handleClick}>
-						<i className={click ? 'fas fa-times' : 'fas fa-bars'}
-						 />
-						 {/* toggle if not clicked will be hamburger menu, on click will be the bars */}
-						</div>
+					<div className="menu-icon" onClick={handleClick}>
+						<i className={click ? "fas fa-times" : "fas fa-bars"} />
+						{/* toggle if not clicked will be hamburger menu, on click will be the bars */}
+					</div>
+					<ul className={click ? "nav-menu active" : "nav-menu"}>
+						<li className="nav-item">
+							<Link to="/" className="nav-links" onClick={closeMobileMenu}>
+								Home
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/work" className="nav-links" onClick={closeMobileMenu}>
+								Work
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+								About
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link
+								to="/contact"
+								className="nav-links"
+								onClick={closeMobileMenu}
+							>
+								Contact
+							</Link>
+						</li>
+					</ul>
 				</div>
 			</nav>
 		</>
