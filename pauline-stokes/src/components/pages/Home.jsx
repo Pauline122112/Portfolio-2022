b92dc1bfa8023eb8a3ee77271d8ac9f670f3cada
Pauline122112 <pauline.design007@gmail.com>
2022-03-12 13:20:08 -0600

    added CardItem.jsx and cards.jsx-still needs refactoring asap

--- pauline-stokes/src/components/pages/Home.jsx
+++ pauline-stokes/src/components/pages/Home.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import '../../App.css';
 import MainSection from '../MainSection';
+import Cards from '../Cards';
 
 
 function Home() {
   return (
     <>
       <MainSection />
+      <Cards />
      
     </>
   );
