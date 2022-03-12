b1919e1fcfab728adf51436e5057ced713431e28
Pauline122112 <pauline.design007@gmail.com>
2022-03-12 13:42:36 -0600

    added pages.jsx to app.js

--- pauline-stokes/src/App.js
+++ pauline-stokes/src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css'
 import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Work from './components/pages/Work';
+import SignUp from './components/pages/SignUp';
 
 
 function App() {
@@ -10,9 +13,12 @@ function App() {
 		<>
 			<Router>
 				<Navbar />
-        		<Routes>
-          			<Route path='/' exact element={<Home />} />
-        		</Routes>
+				<Routes>
+					<Route path="/" exact element={<Home />} />
+					<Route path="/about" exact element={<About />} />
+					<Route path="/sign-up" exact element={<SignUp />} />
+					<Route path="/work" exact element={<Work />} />
+				</Routes>
 			</Router>
 		</>
 	);
